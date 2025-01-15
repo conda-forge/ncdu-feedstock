97139b9b981b3bf1e153bf9db591ea715605f7ea
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-15 10:57:59 -0800

    update to 2.6

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
 
-./configure --prefix="${PREFIX}"
-make -j${CPU_COUNT}
-make install
+set -o xtrace -o nounset -o pipefail -o errexit
+
+case ${target_platform} in:
+    linux-64 )
+        zig build --prefix ${PREFIX} -Doptimize=ReleaseFast -Dtarget=x86_64-linux-gnu.2.17 -Dcpu=core2;;
+    linux-aarch64 )
+        zig build --prefix ${PREFIX} -Doptimize=ReleaseFast -Dtarget=aarch64-linux-gnu.2.17 -Dcpu=core2;;
+    osx-64 )
+        zig build --prefix ${PREFIX} -Dpie=true -Doptimize=ReleaseFast -Dcpu=core2;;
+    osx-arm64 )
+        zig build --prefix ${PREFIX} -Dpie=true -Doptimize=ReleaseFast -Dcpu=apple_m1;;
+esac
+
+mkdir -p ${PREFIX}/share/man/man1
+install -m 644 ncdu.1 ${PREFIX}/share/man/man1/ncdu.1
